405897109e35afa48fbdab10e8b8e9559dd1871d
Ilya Komarov <ilyakomarov1171@gmail.com>
2022-11-07 16:20:39 +0300

    Написаны функции для: 1) Получения случайного элемента массива 2) Создания объекта (фотографии) 3) Создания массива из 25 сгенерированных объектов (фотографий) Также указан диапозон лайков, комментариев. Написано описание фотографий.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,28 @@
+let photosAmount = 25
+
+let likesAmount = {
+  min: 15,
+  max: 200
+};
+
+let comments = {
+  min: 0,
+  max: 200
+}
+
+let description = [
+  "На отдыхе.",
+  "Тусим с друзьями.",
+  "Просто крутой кадр.",
+  "А вы тут бывали?",
+  "Хочется поделиться этой красотой.",
+  "Зацените!",
+  "Проводим отпуск с пользой.",
+  "Это было волшебно!",
+  "Хотел бы я тут остаться навсегда.",
+  "Тест новой камеры."
+]
+
 let getRandomInt = function (a, b) {
   if (a > b) {
     return NaN;
@@ -14,3 +39,20 @@ let checkStringLength = function (str, maxLength) {
 };
 
 console.log(checkStringLength("1", 1))
+
+let getRandomArrayElement = (elements) => elements[getRandomInt(0, elements.lenght - 1)]
+
+let createPhotos = (index) => ({
+  id: index,
+  url: 'photos/{index}}.jpg',
+  description: getRandomArrayElement(description),
+  likes: getRandomInt(likesAmount.min, likesAmount.max),
+  comments:  getRandomInt(comments.min, comments.max)
+});
+
+let getPhotos = () =>
+  Array.from({lenght: photosAmount}, (photoIndex) =>
+    createPhotos(photoIndex + 1)
+  );
+
+
