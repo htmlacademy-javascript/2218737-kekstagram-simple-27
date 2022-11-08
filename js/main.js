4a5cda3b85e4969b38aff1caf11dde82603dee48
Ilya Komarov <ilyakomarov1171@gmail.com>
2022-11-08 10:25:52 +0300

    Написаны функции для:
    1) Получения случайного элемента массива
    2) Создания объекта (фотографии)
    3) Создания массива из 25 сгенерированных объектов (фотографий)
    
    Также указан диапозон лайков, комментариев. Написано описание фотографий.

--- js/main.js
+++ js/main.js
@@ -1,58 +1,60 @@
-let photosAmount = 25
+const PHOTOS_AMOUNT = 25;
 
-let likesAmount = {
+const LikesAmount = {
   min: 15,
   max: 200
 };
 
-let comments = {
+const Comments = {
   min: 0,
   max: 200
-}
+};
 
-let description = [
-  "На отдыхе.",
-  "Тусим с друзьями.",
-  "Просто крутой кадр.",
-  "А вы тут бывали?",
-  "Хочется поделиться этой красотой.",
-  "Зацените!",
-  "Проводим отпуск с пользой.",
-  "Это было волшебно!",
-  "Хотел бы я тут остаться навсегда.",
-  "Тест новой камеры."
-]
+const DESCRIPTION = [
+  'На отдыхе.',
+  'Тусим с друзьями.',
+  'Просто крутой кадр.',
+  'А вы тут бывали?',
+  'Хочется поделиться этой красотой.',
+  'Зацените!',
+  'Проводим отпуск с пользой.',
+  'Это было волшебно!',
+  'Хотел бы я тут остаться навсегда.',
+  'Тест новой камеры.'
+];
 
-let getRandomInt = function (a, b) {
+const getRandomInt = function (a, b) {
   if (a > b) {
     return NaN;
   }
 
-  let random = Math.random(); // от 0 до 1
+  const random = Math.random(); // от 0 до 1
   return random * (b - a) + a ; // от а до а+1
 };
 
-console.log(getRandomInt(201,200))
+getRandomInt();
 
-let checkStringLength = function (str, maxLength) {
-  return str.length <= maxLength
+const checkStringLength = function (str, maxLength) {
+  return str.length <= maxLength;
 };
 
-console.log(checkStringLength("1", 1))
+checkStringLength();
 
-let getRandomArrayElement = (elements) => elements[getRandomInt(0, elements.lenght - 1)]
+const getRandomArrayElement = (elements) => elements[getRandomInt(0, elements.lenght - 1)];
 
-let createPhotos = (index) => ({
+const createPhotos = (index) => ({
   id: index,
   url: 'photos/{index}}.jpg',
-  description: getRandomArrayElement(description),
-  likes: getRandomInt(likesAmount.min, likesAmount.max),
-  comments:  getRandomInt(comments.min, comments.max)
+  description: getRandomArrayElement(DESCRIPTION),
+  likes: getRandomInt(LikesAmount.min, LikesAmount.max),
+  comments:  getRandomInt(Comments.min, Comments.max)
 });
 
-let getPhotos = () =>
-  Array.from({lenght: photosAmount}, (photoIndex) =>
+const getPhotos = () =>
+  Array.from({lenght: PHOTOS_AMOUNT}, (_, photoIndex) =>
     createPhotos(photoIndex + 1)
   );
 
 
+createPhotos();
+getPhotos();
