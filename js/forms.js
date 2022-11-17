ac5f067c1eb60058bf4afa5ca14a969ef4b93ee2
Ilya Komarov <ilyakomarov1171@gmail.com>
2022-11-17 23:30:02 +0300

    Подключен pristine, заданы правильные атрибуты формам в index.html Написаны события по изменению, закрытию и отправке формы

---
+++ js/forms.js
@@ -0,0 +1,53 @@
+const form = document.querySelector('.img-upload__form');
+const overlay = document.querySelector('.img-upload__overlay');
+const body = document.querySelector('body');
+const cancelButton = document.querySelector('#upload-cancel');
+const fileField = document.querySelector('#upload-file');
+const commentField = document.querySelector('.text__description');
+
+const pristine = new Pristine(form, {
+  classTo: '.img-upload__field-wrapper',
+  errorTextParent: '.img-upload__field-wrapper',
+  errorTextClass: '.img-upload__field-wrapper__error'
+});
+
+const showModal = () => {
+  overlay.classList.remove('hidden');
+  body.classList.add('modal-open');
+  document.addEventListener('keydown', onEscKeyDown);
+};
+
+const hideModal = () => {
+  form.reset();
+  pristine.reset();
+  overlay.classList.add('hidden');
+  body.classList.remove('modal-open');
+  document.removeEventListener('keydown', onEscKeyDown);
+};
+
+const isTextFieldFocused = () =>
+  document.activeElement === commentField;
+
+function onEscKeyDown(evt) {
+  if (evt.key === 'Escape' && !isTextFieldFocused()) {
+    evt.preventDefault();
+    hideModal();
+  }
+}
+
+const onCancelButtonClick = () => {
+  hideModal();
+};
+
+const onFileInputChange = () => {
+  showModal();
+};
+
+const onFormSubmit = (evt) => {
+  evt.preventDefault();
+  pristine.validate();
+};
+
+fileField.addEventListener('change', onFileInputChange);
+cancelButton.addEventListener('click', onCancelButtonClick);
+form.addEventListener('submit', onFormSubmit);
